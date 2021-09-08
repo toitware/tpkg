bac8d2ea2147c2d103355957f2f406c5ff7dae14
Florian Loitsch <florian@toitware.com>
2021-09-08 11:14:36 +0200

    Fix GitRegistryNotSynced test.
    That test relies on the paths of cached registries and packages.

--- tests/pkg_test.go
+++ tests/pkg_test.go
@@ -989,9 +989,11 @@ func test_toitPkg(t *tedi.T) {
 			{"pkg", "registry", "add", "test-reg", regPath},
 		})
 		// Delete the registry cache.
-		err := os.RemoveAll(filepath.Join(pt.registryCacheDir, regPath))
+		escapedRegistry := filepath.FromSlash(string(path.ToEscapedURLPath(filepath.ToSlash(regPath))))
+		err := os.RemoveAll(filepath.Join(pt.registryCacheDir, escapedRegistry))
 		assert.NoError(t, err)
-		err = os.RemoveAll(filepath.Join(pt.pkgCacheDir, regPath))
+		escapedPkgPath := pt.computePathInCache(regPath, "", "")
+		err = os.RemoveAll(filepath.Join(pt.pkgCacheDir, escapedPkgPath))
 		assert.NoError(t, err)
 
 		pt.GoldToit("test-2", [][]string{
