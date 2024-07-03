fc61a5d69a355545b032e282f393bf70d80bf860
Florian Loitsch <florian@toit.io>
2024-07-03 10:49:03 +0200

    Debugging...

--- pkg/toitdoc/generator.go
+++ pkg/toitdoc/generator.go
@@ -6,6 +6,7 @@ package toitdoc
 
 import (
 	"context"
+	"os"
 	"os/exec"
 
 	"github.com/toitlang/tpkg/pkg/tpkg"
@@ -25,6 +26,33 @@ func provideGenerator(cfg *config.Config, logger *zap.Logger) *generator {
 	}
 }
 
+// func (g *generator) generateDocs(ctx context.Context, projectPath string, desc *tpkg.Desc, outFile string) error {
+// 	cmd := exec.CommandContext(ctx, g.cfg.ToitlspPath(),
+// 		"toitdoc",
+// 		"--toitc", g.cfg.ToitcPath(),
+// 		"--sdk", g.cfg.Path,
+// 		"--exclude-sdk",
+// 		"--pkg-name", desc.Name,
+// 		"--version", desc.Version,
+// 		"--out", outFile,
+// 		"./src",
+// 	)
+// 	cmd.Dir = projectPath
+
+// 	g.logger.Debug("generating toitdocs", zap.String("cwd", projectPath), zap.String("cmd", cmd.String()), zap.String("url", desc.URL), zap.String("version", desc.Version))
+// 	if err := cmd.Run(); err != nil {
+// 		var stdout string
+// 		if exitErr, ok := err.(*exec.ExitError); ok {
+// 			stdout = string(exitErr.Stderr)
+// 		}
+// 		g.logger.Error("failed to generate toitdocs", zap.String("stdout", stdout), zap.String("cwd", projectPath), zap.String("cmd", cmd.String()), zap.Error(err), zap.String("url", desc.URL), zap.String("version", desc.Version))
+// 		return err
+// 	}
+
+// 	g.logger.Debug("generated toitdocs", zap.String("cwd", projectPath), zap.String("cmd", cmd.String()), zap.String("url", desc.URL), zap.String("version", desc.Version))
+// 	return nil
+// }
+
 func (g *generator) generateDocs(ctx context.Context, projectPath string, desc *tpkg.Desc, outFile string) error {
 	cmd := exec.CommandContext(ctx, g.cfg.ToitlspPath(),
 		"toitdoc",
@@ -38,13 +66,13 @@ func (g *generator) generateDocs(ctx context.Context, projectPath string, desc *
 	)
 	cmd.Dir = projectPath
 
-	g.logger.Debug("generating toitdocs", zap.String("cwd", projectPath), zap.String("cmd", cmd.String()), zap.String("url", desc.URL), zap.String("version", desc.Version))
+	// Inherit stdout and stderr from the current process
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	g.logger.Debug("generating toitdocs x", zap.String("cwd", projectPath), zap.String("cmd", cmd.String()), zap.String("url", desc.URL), zap.String("version", desc.Version))
 	if err := cmd.Run(); err != nil {
-		var stdout string
-		if exitErr, ok := err.(*exec.ExitError); ok {
-			stdout = string(exitErr.Stderr)
-		}
-		g.logger.Error("failed to generate toitdocs", zap.String("stdout", stdout), zap.String("cwd", projectPath), zap.String("cmd", cmd.String()), zap.Error(err), zap.String("url", desc.URL), zap.String("version", desc.Version))
+		g.logger.Error("failed to generate toitdocs", zap.String("cwd", projectPath), zap.String("cmd", cmd.String()), zap.Error(err), zap.String("url", desc.URL), zap.String("version", desc.Version))
 		return err
 	}
 
